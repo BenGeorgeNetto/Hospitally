616f3e981affec9f9f343b91423e6e30414de28b
Ben George Netto <bengnetto245@gmail.com>
2023-07-09 01:03:54 +0530

    add settings

---
+++ app/src/main/java/com/example/hospitally/ui/screens/SettingsScreen.kt
@@ -0,0 +1,36 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import com.example.hospitally.ui.components.CustomTopBar
+
+
+@Composable
+fun SettingsScreen(
+    navController: NavController
+) {
+    Scaffold(topBar = {
+        CustomTopBar(
+            text = "Settings",
+            navController
+        )
+    }) {
+        Surface(
+            modifier = Modifier.padding(it)
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+            ) {
+
+            }
+        }
+    }
+}
