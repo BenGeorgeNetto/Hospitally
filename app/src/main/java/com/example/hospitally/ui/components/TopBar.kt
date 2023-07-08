60c1a6d0cca419974829309cf2b419dda746d97a
Ben George Netto <bengnetto245@gmail.com>
2023-07-09 01:03:39 +0530

    add back button to topbar

--- app/src/main/java/com/example/hospitally/ui/components/TopBar.kt
+++ app/src/main/java/com/example/hospitally/ui/components/TopBar.kt
@@ -1,19 +1,25 @@
 package com.example.hospitally.ui.components
 
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CustomTopBar(
-    text: String
+    text: String,
+    navController: NavController
 ) {
     CenterAlignedTopAppBar(
         title = {
@@ -25,5 +31,15 @@ fun CustomTopBar(
                 modifier = Modifier.padding(8.dp),
             )
         },
+        navigationIcon = {
+            IconButton(onClick = {
+                navController.popBackStack()
+            }) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "Back Button",
+                )
+            }
+        }
     )
 }
